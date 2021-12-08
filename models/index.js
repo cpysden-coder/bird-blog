20cbbb2b920d7ae3941832e5c2c3bffbb483eb37
cpysden-coder <chris_pysden@mac.com>
2021-12-07 18:46:30 -0800

    comment out model post comment

--- models/index.js
+++ models/index.js
@@ -11,13 +11,13 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Post.hasMany(Comment,  {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
-})
+// Post.hasMany(Comment,  {
+//     foreignKey: 'post_id',
+//     onDelete: 'CASCADE'
+// })
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-})
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id'
+// })
 
 module.exports = { User, Post, Comment };
